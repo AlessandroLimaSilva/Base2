41ab667c7d2efc311a99c0513919ab1aa68f8c61
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-06-17 12:27:49 -0300

    Refatoração de encapsulação metodos de acesso a variaveis

--- src/test/java/model/PageRegistroMudanca.java
+++ src/test/java/model/PageRegistroMudanca.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebDriver;
 public class PageRegistroMudanca {
 
 	private String urlRegistroMudanca = "https://mantis.saojudas.base2.com.br/changelog_page.php";
-	public By botaoRegistroMudanca = By.xpath("/html/body/div[2]/div[1]/ul/li[4]/a/span");
-	public WebDriver driver;
+	private By botaoRegistroMudanca = By.xpath("/html/body/div[2]/div[1]/ul/li[4]/a/span");
+	private WebDriver driver;
 	
 	public PageRegistroMudanca(WebDriver browser) {
 		this.driver = browser;
